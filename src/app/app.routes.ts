604960c238b2ff77cfb0fe6e2662dad5684f4fcc
Pseguel27 <pato.seguel.moya@gmail.com>
2018-12-05 21:27:59 -0300

    se cambio el template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeWComponent } from "./components/home-w/home-w.component";
-import { Form1WComponent } from "./components/form1-w/form1-w.component";
-import { Form2WComponent } from "./components/form2-w/form2-w.component";
-
 
 const APP_ROUTES: Routes = [
 	{ path: 'home', component: HomeWComponent },
-	{ path: 'form1', component: Form1WComponent },
-	{ path: 'form2', component: Form2WComponent },
 	{ path: '', component: HomeWComponent, pathMatch: 'full' },
 	{ path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash:true } );
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash:false } );
--- src/app/components/home-w/home-w.component.ts
+++ src/app/components/home-w/home-w.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
 
 @Component({
   selector: 'app-home-w',
   templateUrl: './home-w.component.html',
-  styleUrls: ['./home-w.component.css']
+  styleUrls: ['./home-w.component.css'],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}]
 })
 export class HomeWComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+    
   }
 
 }
